0733d3aac5e2c3c7cc3317057024dbd7c21c960a
hollo <holloreka1@gmail.com>
2021-05-03 17:13:28 +0200

    basic-ts-react: add basic hook

--- 07-react-hook-example/react-hook-example/src/App.js
+++ 07-react-hook-example/react-hook-example/src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>React hook example</h1>
-    </div>
-  );
+// const { Provider, Consumer } = React.createContext();
+const Store = React.createContext();
+
+// Provider
+export function Parent(props) {
+  const obj = {text: "random text"};
+  return <Store.Provider value={obj}>{props.children}</Store.Provider>
 }
 
-export default App;
+// Consumer
+export function Child() {
+  const hook = React.useContext(Store);
+  return <div>{hook.text}</div>;
+}
