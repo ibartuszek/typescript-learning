d2cdc904e9b4b9904c290b6595837ce0e7cf4cf5
hollo <holloreka1@gmail.com>
2021-05-03 21:04:12 +0200

    basic-ts-react: clean up, renam index.js to index.tsx, add store

--- 08-rick-and-morty-episode-picker/src/index.tsx
+++ 08-rick-and-morty-episode-picker/src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {StoreProvider} from "./Store"
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
